e94188804ca4d08debebef3ad8866ec16d8302f4
osycon <fredrichawk@gmail.com>
2017-05-09 07:14:51 +0200

    Add redux  store

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
 import css from './style.scss';
 import App from './Components/App';
+
+const store = createStore();
 
 ReactDOM.render(<App />, document.getElementById(`root`));
