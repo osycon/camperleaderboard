8134d5098be207080034ecff08306dd64e4fafea
osycon <fredrichawk@gmail.com>
2017-05-10 07:54:48 +0200

    Change prepaired userReducer for change

--- src/Reducers/reducers.js
+++ src/Reducers/reducers.js
@@ -1,5 +1,14 @@
 const userReducer = function(state = {}, action) {
-  return state;
+  switch (action.type) {
+    case `30days`:
+      // change state to return recent users
+      return state;
+    case `alltime`:
+      // return all time users
+      return state;
+    default:
+      return state;
+  }
 };
 
 const selectedUsersReducer = function(state = {}, action) {
