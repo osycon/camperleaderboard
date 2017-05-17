f8874b667a9238351ea1dcc00761954909cc8933
osycon <fredrichawk@gmail.com>
2017-05-17 19:01:03 +0200

    Change implementation of reducers

--- src/Reducers/reducers.js
+++ src/Reducers/reducers.js
@@ -1,24 +1,26 @@
-import api from '../utils/api';
-
-const userReducer = (state = {}, action) => {
+export function usersRecent(state = [], action) {
   switch (action.type) {
     case `RECENT`:
-      // change state to return recent users
-      return api.getLast30Days().then(users => {
-        return {
-          users,
-          selectedUsers: `recent`
-        };
-      });
-    case `ALLTIME`:
-      // return all time users
-      return api.getAllTime().then(users => {
-        return {
-          users,
-          selectedUsers: `alltime`
-        };
-      });
+      return action.usersRecent;
     default:
       return state;
   }
-};
+}
+
+export function usersAllTime(state = [], action) {
+  switch (action.type) {
+    case `ALLTIME`:
+      return action.usersAllTime;
+    default:
+      return state;
+  }
+}
+
+export function users(state = `recent`, action) {
+  switch (action.type) {
+    case `SELECTED_USERS`:
+      return action.selectedUsers;
+    default:
+      return state;
+  }
+}
